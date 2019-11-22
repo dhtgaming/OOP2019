eb25a71bdcddd70c7dc285d3af3fb19bf1508223
dhtgaming <47740790+dhtgaming@users.noreply.github.com>
2019-11-22 11:37:59 +0100

    Create IMapa.cs

---
+++ Lesson4/IMapa.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zaliczenie
+{
+    interface IMapa
+    {
+        public void generate();
+        public void list();
+        public void show();
+    }
+}

e02a8ffcf9ec168468d4ea94c9a55842731885e1
dhtgaming <47740790+dhtgaming@users.noreply.github.com>
2019-11-22 11:37:45 +0100

    Create Mapa.cs

---
+++ Lesson4/Mapa.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Zaliczenie
+{
+    class Mapa:, IMapa
+    {
+        public int ShowMap(int wielkosc)
+        {
+            int[]rozmiar = new int[wielkosc];
+        }
+    }
+
+    class Point
+    {
+        public int x;
+        public int y;
+
+        public void print()
+        {
+            Console.WriteLine("X" + x);
+            Console.WriteLine("Y" + y);
+        }
+    }
+
+    class MapElement:Point
+    {
+        public char icon;
+        public void print()
+        {
+            Console.WriteLine("Icon" + icon);
+        }
+    }
+
+    class Grass:MapElement
+    {
+        public int trawa;
+        MapElement G = new MapElement();
+        public void print()
+        {
+            Console.WriteLine("Współrzędne" + (trawa + x + y));
+        }
+    }
+
+    class Tree:MapElement
+    {
+        public int drzewo;
+        MapElement T = new MapElement();
+        public void print()
+        {
+            Console.WriteLine("Współrzędne" + (drzewo + x + y));
+        }
+
+    }
+
+    class Rock:MapElement
+    {
+        public int kamień;
+        MapElement R = new MapElement();
+        public void print()
+        {
+            Console.WriteLine("Współrzędne" + (kamień + x + y));
+        }
+    }
+
+
+}
